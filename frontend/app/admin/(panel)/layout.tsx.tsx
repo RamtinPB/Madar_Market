bd5f1e6e2a77b7ab12df8e6f835ae6fc2cf6800e
RamtinPB <ramtin.panahibazaz@gmail.com>
2025-12-07 22:16:18 +0330

    refactor(admin): simplify admin panel layout and add login header

--- frontend/app/admin/(panel)/layout.tsx.tsx
+++ frontend/app/admin/(panel)/layout.tsx.tsx
@@ -1,4 +1,3 @@
-import { Footer } from "@/src/components/Footer_comp/footer";
 import { Header } from "@/src/components/Header_comp/header";
 
 export default function AdminLayout({
@@ -7,16 +6,8 @@ export default function AdminLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<div className="flex min-h-screen bg-zinc-50 dark:bg-black font-sans">
-			<main className="relative flex min-h-screen w-full max-w-3xl flex-col bg-white dark:bg-black">
-				{/* HEADER */}
-				<Header />
-
-				{/* MAIN CONTENT WRAPPER */}
-				<section className="mt-[84px] mb-[74px] flex w-full justify-center px-6 max-[376px]:px-2.5 max-[321px]:px-1">
-					{children}
-				</section>
-			</main>
+		<div className="relative flex w-full flex-col gap-14 bg-white dark:bg-black overflow-hidden">
+			{children}
 		</div>
 	);
 }
--- frontend/app/admin/(panel)/page.tsx
+++ frontend/app/admin/(panel)/page.tsx
@@ -1,3 +1,13 @@
+import { LoginHeader } from "@/src/components/Login_comp/LoginHeader";
+
 export default function Page() {
-	return;
+	return (
+		<>
+			{/* HEADER */}
+			<LoginHeader />
+
+			{/* MAIN CONTENT */}
+			<main className="flex justify-between items-center"></main>
+		</>
+	);
 }
